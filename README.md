e22bb057cd316ee42baed56da7345c897d68dd7f
Gamerhythm <jakejag@me.com>
2017-03-03 16:38:42 +0000

    Initial release

---
+++ README.md
@@ -0,0 +1,6 @@
+# Slash
+
+This is a semi-realistic AI bot based on ChatterBot with a built in Nazi filter. It also comes with a built in database from when I ran the bot for around a day, even though you can add your own and it will automatically add to that database the smarter your bot gets. To activate the bot, mention your bot at the beginning of the message.
+# Setup
+
+Setup is extremely simple. Firstly, ensure you have Python 3.6 and pip installed. Then, create a bot user on the Discord developers website and then put the token in token.txt. Then, simply run the bot and it will install everything you need.
---
+++ Slash.py
@@ -0,0 +1,67 @@
+import pip
+import logging
+
+def install(package):
+    pip.main(['install', package])
+
+try:
+    from profanity import profanity
+except:
+    install('profanity')
+    from profanity import profanity
+
+try:
+    from chatterbot.trainers import ChatterBotCorpusTrainer
+    from chatterbot import ChatBot
+except:
+    install('chatterbot')
+    from chatterbot.trainers import ChatterBotCorpusTrainer
+    from chatterbot import ChatBot
+
+try:
+    import discord
+except:
+    install('discord.py')
+    import discord
+
+print("-------------------")
+print("Discord.py version: {}".format(discord.version_info))
+print("-------------------")
+
+logging.basicConfig(level=logging.INFO)
+client = discord.Client()
+
+chatbot = ChatBot("Slash")
+chatbot.set_trainer(ChatterBotCorpusTrainer)
+
+chatbot.train(
+    "chatterbot.corpus.english.greetings",
+    "chatterbot.corpus.english.conversations"
+)
+
+cw = ["hitler", "jew", "nazi", "nigger", "heil"]
+profanity.load_words(cw)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(client.user.mention):
+        SlashResponse = message.content
+        SlashResponse = SlashResponse.replace(client.user.mention, "")
+        if profanity.contains_profanity(SlashResponse):
+            response = """```ERROR: You cannot send controversial messages using this bot.```"""
+        else:
+            response = str(chatbot.get_response(SlashResponse))
+            if profanity.contains_profanity(response):
+                response = str(profanity.censor(response))
+        await client.send_message(message.channel, message.author.mention + " " + response)
+
+@client.event
+async def on_ready():
+    print(' ')
+    print('------------------')
+    print('Logged in as ' + client.user.name)
+    print('ID: ' + client.user.id)
+    print('------------------')
+    print(' ')
+Token = open("token.txt")
+client.run(Token.read())
